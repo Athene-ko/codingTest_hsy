ca5d7dd4f563261aa2102e9bcdd9618a11b4f363
jaieve-ps <jaieve1469@polestarhc.com>
2022-08-26 19:14:05 +0900

    resolve leetcode 383
    HahTable

--- leet/l383.java
+++ leet/l383.java
@@ -1,23 +1,44 @@
+import java.util.HashMap;
+
 public class l383 {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        StringBuilder rnsb = new StringBuilder(ransomNote);
-        char[] letters = magazine.toCharArray();
-        // TODO
-        HashSet<String> hashSet = new HashSet<String>(Arrays.asList(letters.toString()));
-        List<String> list = Arrays.asList(letters.toString());
-        for (String c : hashSet) {
-            if (rnsb.toString().length() == 0) { // 1
+    // Solution1. Hash Table
+    public static boolean canConstruct(String ransomNote, String magazine, boolean expectedAnswer) {
+        int[] letters = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            letters[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--letters[ransomNote.charAt(i) - 'a'] < 0) {
                 return false;
-            } else {
-                if (rnsb.toString().contains(c)) { // true
-                    int idx = rnsb.toString().indexOf(c); // 0
-                    rnsb.deleteCharAt(idx); // rnsb = "a"
-                } else {
-                    return false;
-                }
             }
         }
+
         return true;
 
+    }
+
+    // HashTable Long version
+    public static boolean canConstruct2(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int len = magazine.length();
+
+        for (int i = 0; i < len; i++) {
+            char current = magazine.charAt(i);
+            map.put(current, map.getOrDefault(current, 0) + 1);
+        }
+
+        len = ransomNote.length();
+        for (int i = 0; i < len; i++) {
+            char key = ransomNote.charAt(i);
+            if (map.get(key) != null && map.get(key) > 0) {
+                map.put(key, map.get(key) - 1);
+            } else {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
