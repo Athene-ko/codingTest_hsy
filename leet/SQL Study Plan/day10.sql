425a52f8e02c2dc81d2eabe63b04a111abc788cb
jaieve <yoori0303@naver.com>
2022-09-06 00:15:01 +0900

    add new file

---
+++ leet/SQL Study Plan/day10.sql
@@ -0,0 +1,15 @@
+-- Where
+
+-- 182. Duplicate Emails
+
+
+-- 1050. Actors and Directors who co...
+
+
+-- 1587. Bank ACcount Sumamry II
+
+
+ -- 1084. Sales Analysis III
+
+
+ 
---
+++ leet/SQL Study Plan/day8.sql
@@ -0,0 +1,11 @@
+-- Function!!
+
+-- 586. Customer Placing the Largest
+
+
+-- 511. Game play analysis O
+
+
+-- 1890. The Latest Login in 2020
+
+-- 1741. Find Total Time Spent by Eac...
